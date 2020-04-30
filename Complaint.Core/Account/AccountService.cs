0ebc781898ff36f59ad3bf2ae8085a6f9a410430
Zaryab Haider <z.haider01@outlook.com>
2020-04-30 20:16:49 +0500

    Add project files.

---
+++ Complaint.Core/Account/AccountService.cs
@@ -0,0 +1,27 @@
+﻿using Complaint.Data;
+using Complaint.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Complaint.Core.Account
+{
+    public class AccountService : IAccountService
+    {
+        private readonly RepositoryContext _repositoryContext;
+
+        public AccountService(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+        public bool Login(User user)
+        {
+            User userFound = _repositoryContext.Users.Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefault();
+            if (userFound == null)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ Complaint.Core/ComplaintCore/ComplaintService.cs
@@ -0,0 +1,52 @@
+﻿using Complaint.Data;
+using Complaint.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Complaint.Core.ComplaintCore
+{
+    public class ComplaintService : IComplaintService
+    {
+        private readonly RepositoryContext _repositoryContext;
+
+        public ComplaintService(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+
+        public bool Create(ComplaintModel complaint)
+        {
+            try
+            {
+                _repositoryContext.Complaint.Add(complaint);
+                _repositoryContext.SaveChanges();
+
+                if (complaint.Id > 0)
+                    return true;
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public ComplaintModel FindById(int complaintId)
+        {
+            ComplaintModel complaint = null;
+            try
+            {
+                complaint = _repositoryContext.Complaint.Find(complaintId);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return complaint;
+        }
+    }
+}
